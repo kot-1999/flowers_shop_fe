c64ad7037094cf17af9349e20e83f1f2c5c43915
Oleksandr Kashytskyi <sashakashytskyy@gmail.com>
2026-06-04 18:12:54 +0100

    Route to update app settings was created

---
+++ app/api/cookie/settings/route.ts
@@ -0,0 +1,30 @@
+import {NextRequest, NextResponse} from 'next/server';
+import {getCookie, setCookie} from "@/app/utils/serverFunctions";
+import {CookieKey} from "@/app/utils/enums";
+
+export async function GET() {
+    try {
+        const settings = await getCookie(CookieKey.Settings);
+
+        return NextResponse.json(settings ?? null);
+    } catch (error: any) {
+        return NextResponse.json({ message: error?.message ?? 'Failed to get cookie', status: 500 });
+    }
+}
+
+export async function POST(request: NextRequest) {
+    try {
+        const [body, settings] = await Promise.all([request.json(), getCookie(CookieKey.Settings)])
+
+        const data = {
+            ...settings,
+            ...body
+        }
+        await setCookie(CookieKey.Settings, data, 60 * 60 * 24 * 365);
+
+        return NextResponse.json(data, {status: 200});
+
+    } catch (error: any) {
+        return NextResponse.json({ message: error?.message ?? 'Failed to set cookie', status: 500 });
+    }
+}
