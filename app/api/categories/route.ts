8e95ee82edd7e4bf6fea2c80199c69d2c6e74c29
Oleksandr Kashytskyi <sashakashytskyy@gmail.com>
2026-06-12 00:04:06 +0100

    Categories api routes were added

---
+++ app/api/categories/route.ts
@@ -0,0 +1,29 @@
+import { NextRequest, NextResponse } from 'next/server'
+import { getRequiredHeaders } from '@/app/utils/serverFunctions'
+
+const BACKEND_URL = process.env.BACKEND_URL
+
+export async function GET(req: NextRequest) {
+    try {
+        const headers = await getRequiredHeaders(req)
+
+        const response = await fetch(
+            `${BACKEND_URL}/v1/categories`,
+            {
+                method: 'GET',
+                headers,
+            }
+        )
+
+        const data = await response.json()
+
+        return NextResponse.json(data, {
+            status: response.status,
+        })
+    } catch (error: any) {
+        return NextResponse.json(
+            { message: error.message },
+            { status: 500 }
+        )
+    }
+}
