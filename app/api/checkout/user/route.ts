a67fb97cd49375023f410c0041fdeea2e1a2951b
Oleksandr Kashytskyi <sashakashytskyy@gmail.com>
2026-07-05 22:36:09 +0100

    shipping and checkout routes were added

---
+++ app/api/checkout/user/route.ts
@@ -0,0 +1,32 @@
+import { NextRequest, NextResponse } from 'next/server'
+
+import { getRequiredHeaders } from '@/app/utils/serverFunctions'
+
+const BACKEND_URL = process.env.BACKEND_URL
+
+export async function POST(req: NextRequest) {
+    try {
+        const headers = await getRequiredHeaders(req)
+        const body = await req.json()
+
+        const response = await fetch(
+            `${BACKEND_URL}/api/v1/checkout/user`,
+            {
+                method: 'POST',
+                headers,
+                body: JSON.stringify(body)
+            }
+        )
+
+        const data = await response.json()
+
+        return NextResponse.json(data, {
+            status: response.status
+        })
+    } catch (error: any) {
+        return NextResponse.json(
+            { message: error.message },
+            { status: 500 }
+        )
+    }
+}
