fd1a11cb205e1938246d007fc6b34bfe1cc1027c
Oleksandr Kashytskyi <sashakashytskyy@gmail.com>
2026-07-06 01:25:10 +0100

    Address modal was updated to use ref and to be displayed as a plain form

--- app/components/profile/AddressModal.tsx
+++ app/components/profile/AddressModal.tsx
@@ -1,19 +1,24 @@
 'use client'
 
 import { Modal, Form, Input, Checkbox, Select, message } from 'antd'
-import { useEffect } from 'react'
+import { forwardRef, useEffect, useImperativeHandle, useState } from 'react'
 
-import { Country } from '@/app/utils/enums'
-import { checkRes, getTFunc } from '@/app/utils/helpers'
+import { Country, LocalStorageKey } from '@/app/utils/enums'
+import { checkRes, getLocalStorage, getTFunc } from '@/app/utils/helpers'
 
-export default function AddressModal({
-    open,
-    address,
-    onClose,
-    onSuccess
-}: any) {
+const AddressModal = forwardRef(function AddressModal(
+    {
+        open,
+        address,
+        onClose,
+        onSuccess,
+        useForm = false
+    }: any,
+    ref
+) {
     const [form] = Form.useForm()
     const t = getTFunc()
+    const [addressID, setAddressID] = useState(address?.id)
 
     useEffect(() => {
         if (address) {
@@ -23,13 +28,33 @@ export default function AddressModal({
         }
     }, [address, open])
 
+    useImperativeHandle(ref, () => ({
+        submit: async () => {
+            try {
+                const values = await form.validateFields()
+                await handleSubmit(values)
+            } catch {}
+        },
+        getValues: () => ({
+            ...form.getFieldsValue(),
+            id: addressID
+        })
+    }))
+
     const handleSubmit = async (values: any) => {
         try {
+
+            const checkoutToken = getLocalStorage(LocalStorageKey.CheckoutToken)
+            const headers: any = {
+                'Content-Type': 'application/json'
+            }
+            if (checkoutToken) {
+                headers['Authorization'] = `Bearer ${checkoutToken}`
+            }
+
             const res = await fetch('/api/addresses', {
                 method: 'PUT',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
+                headers,
                 body: JSON.stringify({
                     addressID: address?.id,
                     ...values
@@ -37,9 +62,9 @@ export default function AddressModal({
             })
 
             const data = await res.json()
-
             const isSuccess = await checkRes(res, data, t('Failed to save address'))
 
+            setAddressID(data.address.id)
             if (isSuccess) {
                 onSuccess?.()
             }
@@ -47,6 +72,49 @@ export default function AddressModal({
         } catch {
             message.error(t('Failed to save address'))
         }
+    }
+
+    const FormContent = (
+        <Form form={form} layout="vertical" onFinish={handleSubmit}>
+            <Form.Item name="country" label={t('Country')} required>
+                <Select
+                    options={Object.values(Country).map((val) => ({
+                        value: val,
+                        label: val
+                    }))}
+                />
+            </Form.Item>
+
+            <Form.Item name="city" label={t('City')} required>
+                <Input />
+            </Form.Item>
+
+            <Form.Item name="street" label={t('Street')} required>
+                <Input />
+            </Form.Item>
+
+            <Form.Item name="building" label={t('Building')} required>
+                <Input />
+            </Form.Item>
+
+            <Form.Item name="apartment" label={t('Apartment')}>
+                <Input />
+            </Form.Item>
+
+            <Form.Item name="postcode" label={t('Postcode')} required>
+                <Input />
+            </Form.Item>
+
+            { !useForm
+                && <Form.Item name="isDefault" valuePropName="checked">
+                    <Checkbox>{t('Default address')}</Checkbox>
+                </Form.Item>
+            }
+        </Form>
+    )
+
+    if (useForm) {
+        return FormContent
     }
 
     return (
@@ -57,40 +125,9 @@ export default function AddressModal({
             onOk={() => form.submit()}
             okText={t('Save')}
         >
-            <Form form={form} layout="vertical" onFinish={handleSubmit}>
-                <Form.Item name="country" label={t('Country')} required>
-                    <Select
-                        options={Object.values(Country).map((val ) => ({
-                            value: val,
-                            label: val
-                        }))}
-                    />
-                </Form.Item>
-
-                <Form.Item name="city" label={t('City')} required>
-                    <Input />
-                </Form.Item>
-
-                <Form.Item name="street" label={t('Street')} required>
-                    <Input />
-                </Form.Item>
-
-                <Form.Item name="building" label={t('Building')} required>
-                    <Input />
-                </Form.Item>
-
-                <Form.Item name="apartment" label={t('Apartment')}>
-                    <Input />
-                </Form.Item>
-
-                <Form.Item name="postcode" label={t('Postcode')} required>
-                    <Input />
-                </Form.Item>
-
-                <Form.Item name="isDefault" valuePropName="checked">
-                    <Checkbox>{t('Default address')}</Checkbox>
-                </Form.Item>
-            </Form>
+            {FormContent}
         </Modal>
     )
-}
+})
+
+export default AddressModal
