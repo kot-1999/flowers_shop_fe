ef54c12640faadde002baab5ef7bb4c420a25ef5
Oleksandr Kashytskyi <sashakashytskyy@gmail.com>
2026-07-10 00:23:53 +0100

    Basket items deletion was implemented for unauthenticated user

--- app/api/checkout/order/route.ts
+++ app/api/checkout/order/route.ts
@@ -1,9 +1,11 @@
 import { NextRequest, NextResponse } from 'next/server'
 
 import { CookieKey } from '@/app/utils/enums'
-import { getCookie, getRequiredHeaders } from '@/app/utils/serverFunctions'
+import { getCookie, getRequiredHeaders, setCookie } from '@/app/utils/serverFunctions'
 
 const BACKEND_URL = process.env.BACKEND_URL
+
+const cookieAge = 60 * 60 * 24 * 365
 
 export async function POST(req: NextRequest) {
     try {
@@ -35,6 +37,19 @@ export async function POST(req: NextRequest) {
 
         const data = await response.json()
 
+        if (response.ok && auth) {
+            const updatedBasket = []
+
+            for (const item of basketItems) {
+                const isPurchased = data.order.pricings.find((pricingID: string) => pricingID === item.id)
+
+                if (!isPurchased) {
+                    updatedBasket.push(item)
+                }
+            }
+
+            setCookie(CookieKey.Basket, updatedBasket, cookieAge)
+        }
         return NextResponse.json(data, {
             status: response.status
         })
