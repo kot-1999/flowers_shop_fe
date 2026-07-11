05eeb5e7b84f841a11ef0e225a72bb7ae3a6eaaf
Oleksandr Kashytskyi <sashakashytskyy@gmail.com>
2026-07-11 15:42:20 +0100

    Order routes were added

---
+++ app/api/admin/orders/[orderID]/route.ts
@@ -0,0 +1,34 @@
+import { NextRequest, NextResponse } from 'next/server'
+
+import { getRequiredHeaders } from '@/app/utils/serverFunctions'
+
+const BACKEND_URL = process.env.BACKEND_URL
+
+export async function GET(
+    req: NextRequest,
+    { params }: { params: Promise<{ orderID: string }> }
+) {
+    try {
+        const { orderID } = await params
+        const headers = await getRequiredHeaders(req)
+
+        const response = await fetch(
+            `${BACKEND_URL}/api/v1/admin/orders/${orderID}`,
+            {
+                method: 'GET',
+                headers
+            }
+        )
+
+        const data = await response.json()
+
+        return NextResponse.json(data, {
+            status: response.status
+        })
+    } catch (error: any) {
+        return NextResponse.json(
+            { message: error.message },
+            { status: 500 }
+        )
+    }
+}
