098fd32ff2c6713b526873013ef60baea2431847
Oleksandr Kashytskyi <sashakashytskyy@gmail.com>
2026-07-14 14:43:32 +0100

    refund was added

---
+++ app/api/checkout/order/[orderID]/refund/route.ts
@@ -0,0 +1,47 @@
+import { NextRequest, NextResponse } from 'next/server'
+
+import { getRequiredHeaders } from '@/app/utils/serverFunctions'
+
+const BACKEND_URL = process.env.BACKEND_URL
+
+export async function POST(
+    req: NextRequest,
+    context: {
+        params: Promise<{ orderID: string }>
+    }
+) {
+    try {
+        const { orderID } = await context.params
+
+        const headers: any = await getRequiredHeaders(req)
+
+        const auth = req.headers.get('authorization')
+
+        if (auth) {
+            headers.Authorization = auth
+        }
+
+        const response = await fetch(
+            `${BACKEND_URL}/api/v1/checkout/order/${orderID}/refund`,
+            {
+                method: 'POST',
+                headers
+            }
+        )
+
+        const data = await response.json()
+
+        return NextResponse.json(data, {
+            status: response.status
+        })
+    } catch (error: any) {
+        return NextResponse.json(
+            {
+                message: error.message
+            },
+            {
+                status: 500
+            }
+        )
+    }
+}
